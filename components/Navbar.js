3de630df03c7118b67c2726ef3fe08b082200db0
ramana reddy <gvr.reddycse@gmail.com>
2026-05-15 02:50:37 +0530

    Fixed mobile navbar issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,30 +4,66 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <div className="container">
+
         <Link href="/" className="navbar-brand fw-bold">
           RV Robotics
         </Link>
 
+        {/* Mobile Menu Button */}
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
+        {/* Navbar Links */}
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link href="/" className="nav-link">Home</Link></li>
-            <li className="nav-item"><Link href="/about" className="nav-link">About</Link></li>
-            <li className="nav-item"><Link href="/products" className="nav-link">Products</Link></li>
-            <li className="nav-item"><Link href="/services" className="nav-link">Services</Link></li>
-            <li className="nav-item"><Link href="/gallery" className="nav-link">Gallery</Link></li>
-            <li className="nav-item"><Link href="/blog" className="nav-link">Blog</Link></li>
-            <li className="nav-item"><Link href="/contact" className="nav-link">Contact</Link></li>
+
+            <li className="nav-item">
+              <Link href="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link href="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link href="/services" className="nav-link">
+                Services
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link href="/products" className="nav-link">
+                Products
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link href="/contact" className="nav-link">
+                Contact
+              </Link>
+            </li>
+
           </ul>
+
         </div>
+
       </div>
     </nav>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 
+import { useEffect } from 'react'
+
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
+
   return <Component {...pageProps} />
 }
 
